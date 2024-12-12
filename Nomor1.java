c1e0d26894c63e76bf509b14ba7034d5d5d99345
faridasyraafrjr <undinevrm21@gmail.com>
2024-12-12 13:52:40 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,7 +6,23 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+        static double N (int alas, int tinggi) {
+        return 0.5 * alas * tinggi;
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+      System.out.print("Masukkan alas segitiga: "); 
+      int alas = scanner.nextInt();
+
+      System.out.print("Masukkan tinggi segitiga: ");
+      int tinggi = scanner.nextInt();
+
+      double N = N(alas, tinggi);
+
+      System.out.println("Luas segitiga adalah: %.2f\n" + N);
+    }
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
